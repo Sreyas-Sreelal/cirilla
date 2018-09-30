5c8ed658f947e7c3f084ca0d8fa6ae493161859c
Sreyas <Sreyassreelal@gmail.com>
2018-09-30 13:40:21 +0000

    reduce default timeout

--- types/config.go
+++ types/config.go
@@ -5,6 +5,6 @@ package types
 type Config struct {
 	TelegramToken string `envconfig:"CIRILLA_TOKEN"`
 	Debug         bool   `envconfig:"CIRILLA_DEBUG" default:"false"`
-	Timeout       int    `envconfig:"CIRILLA_TIMEOUT" default:"86400"`
+	Timeout       int    `envconfig:"CIRILLA_TIMEOUT" default:"60"`
 	CommandPrefix string `envconfig:"CIRILLA_CMD_PREFIX" default:"/"`
 }
