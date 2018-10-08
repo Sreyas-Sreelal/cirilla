6c048c734da338d413250155d07ca361854f9879
Sreyas <Sreyassreelal@gmail.com>
2018-10-08 16:15:18 +0000

    add cmd torrent

---
+++ bot/commands/cmd_torrent.go
@@ -0,0 +1,31 @@
+package commands
+
+import (
+	"github.com/Sreyas-Sreelal/cirilla/torrent"
+	"gopkg.in/telegram-bot-api.v4"
+	"log"
+)
+
+func commandTorrent(bot *tgbotapi.BotAPI, args []string, Context bool, update tgbotapi.Update) (err error) {
+	pb := torrent.GetNewClient()
+	var botmsg tgbotapi.MessageConfig
+
+	torrentInfo, err := pb.GetTorrentInfo(args[0])
+	if err != nil {
+		log.Print(err)
+		botmsg = tgbotapi.NewMessage(update.Message.Chat.ID, "Failed to fetch information about that torrent.Try again with precise name")
+		bot.Send(botmsg)
+		return nil
+	}
+
+	InfoMessage := "Name : " + torrentInfo.Name + "\n" + torrentInfo.Description + "\nLink : " + torrentInfo.URL
+	MagnetMessage := torrentInfo.MagnetURL
+
+	botmsg = tgbotapi.NewMessage(update.Message.Chat.ID, InfoMessage)
+	bot.Send(botmsg)
+
+	botmsg = tgbotapi.NewMessage(update.Message.Chat.ID, MagnetMessage)
+	bot.Send(botmsg)
+
+	return nil
+}
