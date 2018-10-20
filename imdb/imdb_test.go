93d803e622e9058209ed63b07b7f5b30e30bc212
Sreyas <Sreyassreelal@gmail.com>
2018-10-20 12:43:01 +0000

    rewrite unit testing

--- imdb/imdb_test.go
+++ imdb/imdb_test.go
@@ -1,43 +1,54 @@
 package imdb
 
 import (
+	"net/http"
 	"reflect"
 	"testing"
 )
 
 func TestMovieClient_GetMovieInfo(t *testing.T) {
-
+	type fields struct {
+		Client *http.Client
+	}
 	type args struct {
 		inputName string
 	}
 	tests := []struct {
-		name string
-
+		name    string
+		fields  fields
 		args    args
 		want    MovieInfo
 		wantErr bool
 	}{
 		{
-			"Testing Starwars",
-			args{"Solo: A Star Wars Story"},
+			"MovieInfo Testing",
+			fields{
+				&http.Client{},
+			},
+			args{
+				"Now you see me",
+			},
 			MovieInfo{
-				"Solo: A Star Wars Story (2018)",
-				"7.1/10",
-				"A Star Wars Story (2018) 7.1/10 During an adventure into the criminal underworld, Han Solo meets his future co-pilot Chewbacca and encounters Lando Calrissian years before joining the Rebellion.",
+				Name:        "Now You See Me (2013)",
+				Description: "An F.B.I. Agent and an Interpol Detective track a team of illusionists who pull off bank heists during their performances, and reward their audiences with the money.",
+				Rating:      "7.3/10",
+				PosterLink:  "https://www.imdb.com/title/tt1670345/mediaviewer/rm1351393536?ref_=tt_ov_i",
 			},
 			false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			mc := GetNewClient()
+			mc := &MovieClient{
+				Client: tt.fields.Client,
+			}
 			got, err := mc.GetMovieInfo(tt.args.inputName)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("MovieClient.GetMovieInfo() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("MovieClient.GetMovieInfo() = %v, want %v", got, tt.want)
+				t.Errorf("MovieClient.GetMovieInfo() = \n%v\n, want \n%v\n", got, tt.want)
 			}
 		})
 	}
