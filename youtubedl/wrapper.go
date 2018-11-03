93cd16f65647903888a576e48f6e2302163de859
Sreyas <Sreyassreelal@gmail.com>
2018-11-03 01:57:30 +0000

    Extract audio from youtube video id

--- youtubedl/wrapper.go
+++ youtubedl/wrapper.go
@@ -11,6 +11,7 @@ type YtOptions struct {
 	Path        string
 	TrackName   string
 	AudioFormat string
+	VideoID     string
 }
 
 //TrackInfo holds Information about track
@@ -22,10 +23,19 @@ type TrackInfo struct {
 //YtGetTrackDetails gets information about a track
 func YtGetTrackDetails(options YtOptions) (*TrackInfo, error) {
 	audioData := &TrackInfo{}
+	var specifier string
+
+	if options.TrackName != "" {
+		specifier = `ytsearch1:"` + options.TrackName + `"`
+	} else if options.VideoID != "" {
+		specifier = options.VideoID
+	} else {
+		return audioData, errors.New("No TrackName or VideoID passed")
+	}
 
 	Output, err := exec.Command(
 		options.Path,
-		`ytsearch1:"`+options.TrackName+`"`,
+		specifier,
 		"-j",
 	).Output()
 
@@ -56,6 +66,29 @@ func YtSearchByName(options YtOptions) (string, error) {
 	err = exec.Command(
 		options.Path,
 		`ytsearch1:"`+options.TrackName+`"`,
+		"--extract-audio",
+		"--audio-format",
+		options.AudioFormat,
+		"--output",
+		audioData.Fulltitle+"."+options.AudioFormat,
+	).Run()
+
+	return audioData.Fulltitle, err
+}
+
+//YtExtractAudioFromID extracts audio and onverts it into desired format
+func YtExtractAudioFromID(options YtOptions) (string, error) {
+	audioData, err := YtGetTrackDetails(options)
+	if err != nil {
+		return "", err
+	}
+	if audioData.Duration > 420 {
+		return "", errors.New("Duration is high")
+	}
+
+	err = exec.Command(
+		options.Path,
+		options.VideoID,
 		"--extract-audio",
 		"--audio-format",
 		options.AudioFormat,
--- youtubedl/wrapper_test.go
+++ youtubedl/wrapper_test.go
@@ -17,12 +17,29 @@ func TestYtGetTrackDetails(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			"Testing nightcore",
+			"Testing by Name",
 			args{
 				YtOptions{
 					"youtube-dl",
 					"nightcore sys stand by you male",
 					"mp3",
+					"",
+				},
+			},
+			&TrackInfo{
+				"Nightcore -  Stand By You (Male)",
+				170,
+			},
+			false,
+		},
+		{
+			"Testing by Videoid",
+			args{
+				YtOptions{
+					"youtube-dl",
+					"",
+					"mp3",
+					"tuIkKaZPGOU",
 				},
 			},
 			&TrackInfo{
@@ -58,12 +75,13 @@ func TestYtSearchByName(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			"Testing nightcore",
+			"Testing song Nightcore -  Stand By You (Male)",
 			args{
 				YtOptions{
 					"youtube-dl",
 					"nightcore sys stand by you male",
 					"mp3",
+					"",
 				},
 			},
 			"Nightcore -  Stand By You (Male)",
@@ -80,6 +98,44 @@ func TestYtSearchByName(t *testing.T) {
 			}
 			if got != tt.want {
 				t.Errorf("YtSearchByName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestYtExtractAudioFromID(t *testing.T) {
+	type args struct {
+		options YtOptions
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			"Testing song Nightcore -  Stand By You (Male)",
+			args{
+				YtOptions{
+					"youtube-dl",
+					"",
+					"mp3",
+					"tuIkKaZPGOU",
+				},
+			},
+			"Nightcore -  Stand By You (Male)",
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := YtExtractAudioFromID(tt.args.options)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("YtExtractAudioFromID() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("YtExtractAudioFromID() = %v, want %v", got, tt.want)
 			}
 		})
 	}
